5c31e25e8c0cc42c6c3c63885eab3d7cd8f09a45
JP <jpmateo@JPMAC.local>
2024-06-09 13:18:43 +0800

    Updated README, Added minor logging capability, and updated test data

--- src/load_balancer.rs
+++ src/load_balancer.rs
@@ -1,6 +1,7 @@
 use pingora::prelude::*;
 use async_trait::async_trait;
 use pingora::lb::LoadBalancer;
+use pingora::protocols::Digest;
 use crate::config::Config;
 use std::sync::{Arc, Mutex};
 use http::uri::Uri;
@@ -20,6 +21,27 @@ impl ProxyHttp for LB {
         ()
     }
 
+
+    async fn connected_to_upstream(&self, 
+        _session: &mut Session,
+        _reused: bool,
+        _peer: &HttpPeer,
+        _fd: std::os::unix::io::RawFd,
+        _digest: Option<&Digest>,
+        _ctx: &mut Self::CTX ) -> Result<()> {
+            let inet = _peer._address.as_inet().unwrap().to_string();
+            match &self.config.load_balancer.debug {
+                status if status == &String::from("INFO") => {
+                    println!("(INFO) [{:?}] - {}",inet,_session.req_header().uri.to_string());
+                }
+                status if status == &String::from("DEBUG") => {
+                    println!("(DEBUG) [{:?}] - {:?}",inet,_session.req_header());
+                }
+                _ => {}
+            };
+            Ok(())
+        }
+
     async fn upstream_peer(
         &self, 
         _session: &mut Session, 
@@ -32,15 +54,18 @@ impl ProxyHttp for LB {
         let backend_routes: Routes = Routes::new(lb_routes_path);
         let backend_mapping: Vec<BackendMapping> = backend_routes.read();    
         let enable_tls_ssl: bool = config.load_balancer.enable_tls_ssl.clone();
+        let is_file: bool = if let Some(_) = _session.req_header().uri_file_extension() { true } else { false };
 
         match self.load_balancer.select_with(b"", 256, |backend,accept| {
             let addr: String = backend.addr.to_string();
             let config: Config = config.clone();
             let mut url_path = url_path.clone();
             
-            if let Some(matched) = self.get_file_in_path(config.load_balancer.allow_file_types_pattern,url_path.clone()) {
-                let file_name = matched.as_str();
-                url_path = url_path.replace(file_name, "").to_string(); 
+            if is_file {
+                if let Some(matched) = self.get_file_in_path(config.load_balancer.allow_file_types_pattern,url_path.clone()) {
+                    let file_name = matched.as_str();
+                    url_path = url_path.replace(file_name, "").to_string(); 
+                }
             }
             
             if let Some(_) = backend_mapping.clone().into_iter().find(|bm| {
